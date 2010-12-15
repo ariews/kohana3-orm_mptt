3bed908f97de594486c66d9449f2c174a43b5bbd
Alexeyco <6425762@gmail.com>
2010-12-15 20:44:33 +0300

    Deep refactoring

--- classes/kohana/mptt.php
+++ classes/kohana/mptt.php
@@ -13,11 +13,6 @@ class Kohana_MPTT extends ORM
 	/**
 	 * @var string
 	 */
-	protected $_id_column = 'id';
-
-	/**
-	 * @var string
-	 */
 	protected $_left_column = 'left';
 
 	/**
@@ -53,8 +48,7 @@ class Kohana_MPTT extends ORM
 	public function __construct($id = NULL)
 	{
 		//We need to determine ORM-rows, that we use
-		foreach(array($this->_id_column,
-			$this->_left_column,
+		foreach(array($this->_left_column,
 			$this->_right_column,
 			$this->_level_column,
 			$this->_parent_column) as $row)
@@ -90,9 +84,9 @@ class Kohana_MPTT extends ORM
 	{
 		if(!$this->loaded())
 		{
-			if($this->parent())
+			if($this->{$this->_parent_column})
 			{
-				return $this->make_child($this->parent());
+				return $this->make_child($this->{$this->_parent_column});
 			}
 			else
 			{
@@ -113,11 +107,10 @@ class Kohana_MPTT extends ORM
 	 */
 	public function make_root($scope = NULL)
 	{
-		// save node as root
+		// Save node as root
 		if($this->loaded()) throw new Kohana_Exception('Cannot insert the same node twice');
 
-		$this->{$this->_level_column} = 1;
-		$this->{$this->_left_column} = 1;
+		$this->{$this->_left_column} = $this->{$this->_level_column} = 1;
 		$this->{$this->_right_column} = 2;
 		$this->{$this->_parent_column} = NULL;
 
@@ -125,7 +118,7 @@ class Kohana_MPTT extends ORM
 	}
 
 	/**
-	 * Creates the child element
+	 * inserts node as direct child for $id node
 	 *
 	 * @param  mixed    ID or parent object
 	 * @param  boolean  First flag
@@ -133,8 +126,8 @@ class Kohana_MPTT extends ORM
 	 */
 	public function make_child($id, $first = FALSE)
 	{
-		// inserts node as direct child for $id node
 		$this->lock();
+
 		if(!is_a($id, get_class($this)))
 		{
 			$id = self::factory($this->_object_name, $id);
@@ -151,8 +144,8 @@ class Kohana_MPTT extends ORM
 
 		$this->add_space($left, 2);
 
-		$this->{$this->_parent_column} = $id->pk();
-		$this->{$this->_level_column} = $id->level() + 1;
+		$this->{$this->_parent_column} = $id->{$this->_primary_key};
+		$this->{$this->_level_column} = $id->{$this->_level_column} + 1;
 		$this->{$this->_left_column} = $left;
 		$this->{$this->_right_column} = $left + 1;
 
@@ -183,11 +176,11 @@ class Kohana_MPTT extends ORM
 
 		if($before)
 		{
-			$left = $id->left();
+			$left = $id->{$this->_left_column};
 		}
 		else
 		{
-			$left = $id->right() + 1;
+			$left = $id->{$this->_right_column} + 1;
 		}
 
 		$this->lock();
@@ -195,17 +188,22 @@ class Kohana_MPTT extends ORM
 
 		$this->{$this->_left_column} = $left;
 		$this->{$this->_right_column} = $left + 1;
-		$this->{$this->_parent_column} = $id->parent();
-		$this->{$this->_level_column} = $id->level();
+		$this->{$this->_parent_column} = $id->{$this->_parent_column};
+		$this->{$this->_level_column} = $id->{$this->_level_column};
 
 		parent::save();
 		$this->unlock();
 		return $this;
 	}
 
+	/**
+	 * Deletes applied node with descendants
+	 *
+	 * @param <type> $id
+	 * @return <type>
+	 */
 	public function delete($id = NULL)
 	{
-		// deletes applied node with descendants
 		if(!is_null($id))
 		{
 			$target = self::factory($this->_object_name, $id);
@@ -219,17 +217,22 @@ class Kohana_MPTT extends ORM
 		$target->lock();
 
 		DB::delete($target->_table_name)
-			->where($target->_left_column, '>=', $target->left())
-			->where($target->_left_column, '<=', $target->right())
+			->where($target->_left_column, '>=', $target->{$this->_left_column})
+			->where($target->_left_column, '<=', $target->{$this->_right_column})
 			->execute($target->_db);
 
-		$target->clear_space($target->left(), $target->size());
+		$target->clear_space($target->{$this->_left_column}, $target->size());
 		$target->unlock();
 	}
 
+	/**
+	 * Moves current node with descendants to a node $id
+	 *
+	 * @param <type> $id
+	 * @param <type> $first
+	 */
 	public function move_to($id, $first = FALSE)
 	{
-		// moves current node with descendants to a node $id
 		if(!is_a($id, get_class($this)))
 		{
 			$id = self::factory($this->_object_name, $id);
@@ -238,14 +241,14 @@ class Kohana_MPTT extends ORM
 		if($this->is_in_descendants($id)) throw new Kohana_Exception('Cannot move nodes to themself');
 
 		$ids = $this->get_subtree(TRUE)->primary_key_array();
-		$left = ($first == TRUE ? $id->left() + 1 : $id->right());
-		$oldlft = $this->left();
-		$level = $id->level() + 1;
-		$delta = $left - $this->left();
+		$left = ($first == TRUE ? $id->{$this->_left_column} + 1 : $id->{$this->_right_column});
+		$oldlft = $this->{$this->_left_column};
+		$level = $id->{$this->_level_column} + 1;
+		$delta = $left - $this->{$this->_left_column};
 
 		if($delta < 0) $delta = '('.$delta.')';
 
-		$deltalevel = $level - $this->level();
+		$deltalevel = $level - $this->{$this->_level_column};
 
 		if($deltalevel < 0) $deltalevel = '('.$deltalevel.')';
 
@@ -264,16 +267,22 @@ class Kohana_MPTT extends ORM
 			))
 			->execute($this->_db);
 
-		$this->{$this->_parent_column} = $id->pk();
+		$this->{$this->_parent_column} = $id->{$this->_primary_key};
 
 		parent::save();
 
 		$this->unlock();
 	}
 
+	/**
+	 * Moves all descendants to $id node WITHOUT current node
+	 *
+	 * @param <type> $id
+	 * @param <type> $first
+	 * @return <type>
+	 */
 	public function move_children_to($id, $first = FALSE)
 	{
-		// moves all descendants to $id node WITHOUT current node
 		if(!$this->has_children()) return FALSE;
 
 		if(!is_a($id, get_class($this)))
@@ -282,14 +291,14 @@ class Kohana_MPTT extends ORM
 		}
 
 		$ids = $this->get_subtree(FALSE)->primary_key_array();
-		$left = ($first == TRUE ? $id->left() + 1 : $id->right());
-		$oldlft = $this->left() + 1;
-		$level = $id->level() + 1;
+		$left = ($first == TRUE ? $id->{$this->_left_column} + 1 : $id->{$this->_right_column});
+		$oldlft = $this->{$this->_left_column} + 1;
+		$level = $id->{$this->_level_column} + 1;
 		$delta = $left - $oldlft;
 
 		if($delta < 0) $delta = '('.$delta.')';
 
-		$deltalevel = $level - $this->level() - 1;
+		$deltalevel = $level - $this->{$this->_level_column} - 1;
 
 		if($deltalevel < 0) $deltalevel = '('.$deltalevel.')';
 
@@ -308,8 +317,8 @@ class Kohana_MPTT extends ORM
 			))->execute($this->_db);
 
 		DB::update($this->_table_name)
-			->set(array($this->_parent_column => $id->pk()))
-			->where($this->_level_column, "=", $id->level() + 1)
+			->set(array($this->_parent_column => $id->{$this->_primary_key}))
+			->where($this->_level_column, "=", $id->{$this->_level_column} + 1)
 			->in($this->primary_key, $ids)
 			->execute($this->_db);
 
@@ -317,166 +326,166 @@ class Kohana_MPTT extends ORM
 		$this->reload();
 	}
 
-	/*
-	 * Retrieving info methods
+	/**
+	 * Returns root of the tree
 	 *
+	 * @param array Scope columns
+	 * @return Kohana_MPTT
 	 */
-
-	public function get_root($scope = NULL)
+	public function get_root(array $scope = NULL)
 	{
-		if(is_null($scope))
+		$root = self::factory($this->_object_name)
+			->where($this->_left_column, '=', 1);
+
+		if($scope !== NULL)
 		{
-			// returns all roots
-			return self::factory($this->_object_name)
-				->where($this->_left_column, "=", 1)
-				->find_all();
+			foreach($scope as $row => $value)
+			{
+				$root = $root->andwhere($row, '=', $value);
+			}
+
+			return $root->find();
 		}
-		else
-		{
-			// only current root
-			return self::factory($this->_object_name)
-				->where($this->_left_column, '=', 1)
-				->find();
-		}
+
+		return $root->find_all();
 	}
 
-	public function get_parents($with_self = FALSE, $columns = FALSE)
+	/**
+	 * Returns aray of parents
+	 *
+	 * @uses $this->_get_somethink();
+	 * @param  boolean  With self
+	 * @param  array    Columns
+	 * @return mixed
+	 */
+	public function get_parents($with_self = FALSE, array $columns = NULL)
 	{
-		$suffix = $with_self ? "= " : " ";
-		if(is_array($columns))
-		{
-			// returns applied columns only
-			$query = DB::select();
-			foreach($columns as $column) $query->select($column);
-
-			return $query
-				->from($this->_table_name)
-				->where($this->_left_column, ' <'.$suffix, $this->left())
-				->where($this->_right_column, ' >'.$suffix, $this->right())
-				->execute($this->_db);
-		}
-		else
-		{
-			// returns all current node parents as ORM objects
-			return self::factory($this->_object_name)
-				->where($this->_left_column, ' <'.$suffix, $this->left())
-				->where($this->_right_column, ' >'.$suffix, $this->right())
-				->find_all();
-		}
+		return $this->_get_somethink(TRUE, $with_self, $columns);
 	}
 
+	/**
+	 * Returns array of children
+	 *
+	 * @uses $this->_get_somethink();
+	 * @param  boolean  With self
+	 * @param  array    Columns
+	 * @return mixed
+	 */
+	public function get_children($with_self = FALSE, array $columns = NULL)
+	{
+		return $this->_get_somethink(FALSE, $with_self, $columns);
+	}
+
+	/**
+	 * Gets parents or children
+	 */
+	protected function _get_somethink($parents, $with_self, $columns)
+	{
+		if(!$this->loaded()) throw new Kohana_Exception('Load model first');
+
+		$suffix = $with_self ? '=' : '';
+
+		$left   = ($parents === TRUE) ? '<' : '>';
+		$right  = ($parents === TRUE) ? '>' : '<';
+
+		$parents = (sizeof($columns) > 0) ? DB::select($columns)->from($this->_table_name) : self::factory($this->_object_name);
+
+		$parents
+			->where($this->_left_column, $left.$suffix, $this->{$this->_left_column})
+			->where($this->_right_column, $right.$suffix, $this->{$this->_right_column});
+
+		// We have scopes?
+		foreach($this->_scope_solumns as $column)
+		{
+			$query = $query->where($column, '=', $this->{$column});
+		}
+
+		return (sizeof($columns) > 0) ? $query->execute($this->_db) : $parents->find_all();
+	}
+
+	/**
+	 * Returns parent object
+	 *
+	 * @return mixed
+	 */
 	public function get_parent()
 	{
-		if($this->is_root()) return NULL;
-		return self::factory($this->_object_name, $this->parent());
-	}
+		// If it is root
+		if($this->is_root()) return FALSE;
 
-	public function get_children()
-	{
-		// returns only direct children
-		return self::factory($this->_object_name)
-			->where($this->_left_column, " >", $this->left())
-			->where($this->_right_column, " <", $this->right())
-			->where($this->_level_column, "=", $this->level() + 1)
-			->find_all();
-	}
-
-	public function get_subtree($with_parent = FALSE)
-	{
-		// return all descendants of current node
-		$suffix = ($with_parent ? '= ' : ' ');
-		return self::factory($this->_object_name)
-			->where($this->_left_column, " >", $suffix . $this->left())
-			->where($this->_right_column, " <", $suffix . $this->right())
-			->find_all();
-	}
-
-	public function get_fulltree($use_scope = TRUE)
-	{
-		// returns full tree (with or without scope checking)
-		$result = self::factory($this->_object_name);
-
-		return ($result->find_all());
-	}
-
-	public function get_leaves()
-	{
-		// returns only leaves of current node
-		return self::factory($this->_object_name)
-			->where($this->_left_column, " >", $suffix . $this->left())
-			->where($this->_right_column, " <", $suffix . $this->right())
-			->where($this->_left_column, "=", DB::expr('"'.$this->_right_column.'" - 1'))
-			->find_all();
+		return self::factory($this->_object_name, $this->{$this->_parent_column});
 	}
 
 	/*
-	 * Simple methods for getting/setting primary info
+	 * Returns full tree (with or without scope checking)
 	 *
+	 * @param array Scopes
 	 */
-
-	public function set_title($title)
+	public function get_fulltree(array $scopes = NULL)
 	{
-		$this->title = $title;
-		return $this;
+		$result = self::factory($this->_object_name);
+
+		foreach($scopes as $column => $value)
+		{
+			$result = $result->where($column, '=', $value);
+		}
+
+		return $result->find_all();
 	}
 
-	public function left()
+	/**
+	 * Returns only leaves of current node
+	 *
+	 * @param  array  Columns
+	 * @return mixed
+	 */
+	public function get_leaves(array $columns = NULL)
 	{
-		return $this->{$this->_left_column};
+		$leaves = (sizeof($columns) > 0) ? DB::select($columns)->from($this->_table_name) : self::factory($this->_object_name);
+
+		$leaves->where($this->_parent_column, '=', $this->{$this->_primary_key});
+
+		return (sizeof($columns) > 0) ? $leaves->execute() : $leaves->find_all();
 	}
 
-	public function right()
-	{
-		return $this->{$this->_right_column};
-	}
-
-	public function level()
-	{
-		return $this->{$this->_level_column};
-	}
-
-	public function parent()
-	{
-		return $this->{$this->_parent_column};
-	}
-
-	public function size()
-	{
-		return $this->{$this->_right_column} - $this->left() + 1;
-	}
-
-	public function count()
-	{
-		return ($this->size() - 2) / 2;
-	}
-
-	public function has_children()
-	{
-		return ($this->size() > 2);
-	}
-
+	/**
+	 * Is current node a direct parent of $id node
+	 *
+	 * @param  mixed $id
+	 * @return boolean
+	 */
 	public function is_parent($id)
 	{
-		// is current node a direct parent of $id node
 		if(!is_a($id, get_class($this)))
 		{
 			$id = self::factory($this->_object_name, $id);
 		}
 
-		return $id->{$this->_parent_column} == $this->pk();
+		return $id->{$this->_parent_column} == $this->{$this->_primary_key};
 	}
 
+	/**
+	 * Is current node a direct child of $id node
+	 *
+	 * @param  mixed $id
+	 * @return boolean
+	 */
 	public function is_child($id)
 	{
-		// is current node a direct child of $id node
 		if(!is_a($id, get_class($this)))
 		{
 			$id = self::factory($this->_object_name, $id);
 		}
 
-		return $this->{$this->_parent_column} == $id->pk();
+		return $this->{$this->_parent_column} == $id->{$this->_primary_key};
 	}
 
+	/**
+	 * Is current node one of a $id node child
+	 *
+	 * @param  mixed $id
+	 * @return boolean
+	 */
 	public function is_in_descendants($id)
 	{
 		// is current node one of a $id node child
@@ -485,46 +494,63 @@ class Kohana_MPTT extends ORM
 			$id = self::factory($this->_object_name, $id);
 		}
 
-		if($this->left() <= $id->left()) return FALSE;
-		if($this->right() >= $id->right()) return FALSE;
+		if($this->{$this->_left_column} <= $id->{$this->_left_column}) return FALSE;
+		if($this->{$this->_right_column} >= $id->{$this->_right_column}) return FALSE;
 
 		return TRUE;
 	}
 
+	/**
+	 * Is current node one of a $id node parents
+	 *
+	 * @param  mixed $id
+	 * @return boolean
+	 */
 	public function is_in_parents($id)
 	{
-		// is current node one of a $id node parents
 		if(!is_a($id, get_class($this)))
 		{
 			$id = self::factory($this->_object_name, $id);
 		}
+
 		return $id->is_in_descendants($this);
 	}
 
+	/**
+	 * Is current node neighbor of $id node (the same direct parent)
+	 *
+	 * @param  mixed $id
+	 * @return boolean
+	 */
 	public function is_neighbor($id)
 	{
-		// is current node neighbor of $id node (the same direct parent)
 		if(!is_a($id, get_class($this)))
 		{
 			$id = self::factory($this->_object_name, $id);
 		}
-		return ($this->parent() == $id->parent());
+
+		return $this->{$this->_parent_column} == $id->{$this->_parent_column};
 	}
 
+	/**
+	 * Is current node a root node?
+	 *
+	 * @return boolean
+	 */
 	public function is_root()
 	{
-		// is current node a root node
-		return $this->level() == 1;
+		return $this->{$this->_level_column} == 1;
 	}
 
-	/*
-	 * Support methods
+	/**
+	 * Add space for adding/inserting nodes.
+	 * It should be set before adding space!
+	 *
+	 * @param integer $start
+	 * @param integer $size
 	 */
-
 	protected function add_space($start, $size = 2)
 	{
-		// add space for adding/inserting nodes
-		// $this->scope should be set before adding space!
 		DB::update($this->_table_name)
 			->set(array($this->_left_column => DB::expr('"'.$this->_left_column.'" + ' . $size)))
 			->where($this->_left_column, " >= ", $start)
@@ -536,47 +562,41 @@ class Kohana_MPTT extends ORM
 			->execute($this->_db);
 	}
 
+	/**
+	 * Remove space after deleting/moving node
+	 *
+	 * @param integer $start
+	 * @param integer $size
+	 */
 	protected function clear_space($start, $size = 2)
 	{
-		// remove space after deleting/moving node
 		DB::update($this->_table_name)
 			->set(array($this->_left_column => DB::expr('"'.$this->_left_column.'" - ' . $size)))
-			->where($this->_left_column, " >= ", $start)
+			->where($this->_left_column, '>=', $start)
 			->execute($this->_db);
 
 		DB::update($this->_table_name)
 			->set(array($this->_right_column => DB::expr('"'.$this->_right_column.'" - ' . $size)))
-			->where($this->_right_column, " >= ", $start)
+			->where($this->_right_column, '>=', $start)
 			->execute($this->_db);
 	}
 
+
+	/**
+	 * Locks table
+	 */
 	protected function lock()
 	{
 		// lock table
 		DB::query('lock', 'LOCK TABLE ' . Kohana::config('database')->default['table_prefix'].$this->_table_name . ' WRITE')->execute($this->_db);
 	}
 
+	/**
+	 * Unlocks table
+	 */
 	protected function unlock()
 	{
 		// unlock tables
 		DB::query('unlock', 'UNLOCK TABLES')->execute($this->_db);
-	}
-
-	public function __get($column)
-	{
-		if($column === 'parent')
-			return $this->get_parent();
-		elseif($column === 'parents')
-			return $this->get_parents();
-		elseif($column === 'children')
-			return $this->get_children();
-		elseif($column === 'leaves')
-			return $this->get_leaves();
-		elseif($column === 'subtree')
-			return $this->get_subtree();
-		elseif($column === 'fulltree')
-			return $this->get_fulltree();
-		else
-			return parent::__get($column);
 	}
 }
