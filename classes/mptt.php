0df793c24809fe8bee9cc48a116fd2a931a47b41
Alexeyco <6425762@gmail.com>
2010-12-15 19:30:45 +0300

    PHPDoc added to mptt.php

--- classes/mptt.php
+++ classes/mptt.php
@@ -1,3 +1,11 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
-
+/**
+ * Its a combination of a traditional MPTT tree and additional usefull data (parent id, level value)
+ *
+ * @see http://dev.mysql.com/tech-resources/articles/hierarchical-data.html
+ *
+ * @package    Leemo
+ * @author     Leemo team
+ * @copyright  Copyright (c) 2010 Leemo team
+ */
 class MPTT extends Kohana_MPTT { }
